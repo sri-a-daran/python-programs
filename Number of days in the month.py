e717799d317a22fcaca88cbac17da1bf17a4d250
sri-a-daran <dsriakash@gmail.com>
2025-10-02 14:41:59 +0530

    Number of days in the month.py

--- Number of days in the month.py
+++ Number of days in the month.py
@@ -5,9 +5,10 @@ if month in (1,3,5,7,8,10,12):
 elif month in (4,6,9,11):
     print("30 days")
 elif month==2:
-    if year%4==0:
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print("29 days")
     else:
         print("28 days")
 else:
     print("Invalid month")
+
